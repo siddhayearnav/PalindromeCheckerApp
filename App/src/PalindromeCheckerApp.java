2d23bd3c5ee573a6cc6794dbc91c6e0998f6a289
lelihanzzza <jeetchamarkumar@gmail.com>
2026-03-13 08:29:41 +0530

    use case 1 done

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,7 +2,7 @@ public class PalindromeCheckerApp{
     public static void main(String[] args) {
         System.out.println("Welcome to the Palindrome Checker Management System" +
                 "\nVersion : 1.0" +
-                "\nSystem initialized successfully");
+                "\nSystem initialized successfully ");
     }
 }
 
